ffb02e108920689efdef1b21c270233bfbb78672
LucHighwalker <luc.highwalker@gmail.com>
2019-05-17 13:31:04 -0700

    did the thing

---
+++ build/precache-manifest.96f1429cdc45330023da6f824c50995b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "84658922405f2af547fc",
+    "url": "./static/css/main.c9e9a8c2.chunk.css"
+  },
+  {
+    "revision": "84658922405f2af547fc",
+    "url": "./static/js/main.3bafa526.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "9cbc7ec7344c99039a12",
+    "url": "./static/js/2.d997cfd3.chunk.js"
+  },
+  {
+    "revision": "e62483e08385c67e5e4f6dc9d4c99dcf",
+    "url": "./index.html"
+  }
+];
